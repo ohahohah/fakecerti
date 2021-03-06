9f2450216ffc9358f2809f6fb3860e33b42a0321
lexifdev <sl@lxf.kr>
2021-03-07 03:54:22 +0900

    메인 URL 협회라이즈

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/Main/main.do')
 def home():
     return render_template('index.html')
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('home')+'?site=main01')
 
 
 if __name__ == '__main__':
